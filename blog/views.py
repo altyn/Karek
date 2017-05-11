6d893f3507741d7728dcf3c0b2b70d0080f94b34
Altynbek Shaidyldaev <altynbek.shaidyldaev@gmail.com>
2017-05-11 16:25:13 +0600

    News and Partners on Index page

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
 from .models import News, Partners, Advert, Founders
 
@@ -6,6 +5,19 @@ from .models import News, Partners, Advert, Founders
 # Create your views here.
 class IndexView(TemplateView):
     template_name = 'base.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        try:
+            context["news_list"] = News.objects.all()
+        except News.DoesNotExist:
+            context["news_list"] = []
+        try:
+            context["partners_list"] = Partners.objects.all()
+        except Partners.DoesNotExist:
+            context["partners_list"] = []
+        print(context)
+        return context
 
 
 class NewsList(ListView):
